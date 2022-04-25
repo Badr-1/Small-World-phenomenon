e730076c2d110f7e8a7c953c9957ad5ac9d54424
Badr-1 <badrmohamedashraf@gmail.com>
2022-04-24 18:37:24 -0700

    established test case run

--- Small World Phenomenon/Program.cs
+++ Small World Phenomenon/Program.cs
@@ -9,25 +9,29 @@ namespace Small_World_Phenomenon
     internal class Program
     {
         static string moviesPath, queriesPath;
+        static Dictionary<string, HashSet<string>> actorsInMovie = new Dictionary<string, HashSet<string>>();
+        static Dictionary<string, HashSet<string>> moviesOfActor = new Dictionary<string, HashSet<string>>();
+        static List<KeyValuePair<string, string>> queries = new List<KeyValuePair<string, string>>();
         static void Main(string[] args)
         {
             while (true)
             {
-                Dictionary<string, List<string>> movies = new Dictionary<string, List<string>>();
-                List<KeyValuePair<string,string>> queries = new List<KeyValuePair<string,string>>();
+               
                 selectTestCase();
-                parseMovies(movies, moviesPath);
-                parseQueries(queries, queriesPath);
-                Console.WriteLine("movies numbers : {0}", movies.Count());
-                Console.WriteLine("queries numbers : {0}", queries.Count());
+                parseMovies();
+                parseQueries();
+                runTestCase(queries);
+                Console.WriteLine("movies number : {0}", actorsInMovie.Count());
+                Console.WriteLine("queries number : {0}", queries.Count());
+                Console.WriteLine("actros number : {0}", moviesOfActor.Count());
                 Console.ReadLine();
                 Console.Clear();
             }
         }
 
-        public static void parseMovies(Dictionary<string, List<string>> dict, string path)
+        public static void parseMovies()
         {
-            StreamReader reader = new StreamReader(path);
+            StreamReader reader = new StreamReader(moviesPath);
             while (true)
             {
                 string line = reader.ReadLine();
@@ -35,21 +39,24 @@ namespace Small_World_Phenomenon
                     break;
 
                 string movie_name = line.Substring(0, line.IndexOf('/'));
-                dict[movie_name] = new List<string>();
+                actorsInMovie[movie_name] = new HashSet<string>();
                 while (true)
                 {
                     line = line.Remove(0, (line.IndexOf('/') != -1) ? line.IndexOf('/') + 1 : line.Length);
                     if (line.Length == 0)
                         break;
                     string actor_name = (line.IndexOf('/') != -1) ? line.Substring(0, line.IndexOf('/')) : line;
-                    dict[movie_name].Add(actor_name);
+                    actorsInMovie[movie_name].Add(actor_name);
+                    if (!moviesOfActor.ContainsKey(actor_name))
+                        moviesOfActor[actor_name] = new HashSet<string>();
+                    moviesOfActor[actor_name].Add(movie_name);
 
                 }
             }
         }
-        public static void parseQueries(List<KeyValuePair<string, string>> query, string path)
+        public static void parseQueries()
         {
-            StreamReader reader = new StreamReader(path);
+            StreamReader reader = new StreamReader(queriesPath);
             while (true)
             {
                 string line = reader.ReadLine();
@@ -58,119 +65,10 @@ namespace Small_World_Phenomenon
                 string key = line.Substring(0, line.IndexOf('/'));
                 line = line.Remove(0, key.Length);
                 string value = line.Substring(1);
-                query.Add(new KeyValuePair<string, string>(key,value));
-                //query[key] = value;
+                queries.Add(new KeyValuePair<string, string>(key,value));
             }
         }
 
-        /*        public static void selectTestCase()
-                {
-                    string path = @"E:\Small_World_henomenon\Testcases\";
-                    string choice;
-                Ask: do
-                    {
-                        Console.WriteLine("1.Sample\t2.Complete");
-                        Console.Write("Select Test Case: ");
-                        choice = Console.ReadLine();
-                    } while (!(choice == "1" || choice == "2"));
-                    switch (choice)
-                    {
-                        case "1":
-                            path += @"Sample\";
-                            moviesPath = path + "movies1.txt";
-                            queriesPath = path + "queries1.txt";
-                            break;
-                        case "2":
-
-                            do
-                            {
-                                Console.WriteLine("1.small\t2.medium");
-                                Console.WriteLine("3.large\t4.extreme");
-                                Console.WriteLine("0.back");
-                                Console.Write("Select Test Case Level: ");
-                                choice = Console.ReadLine();
-
-
-                            } while (!(choice == "1" || choice == "2" || choice == "3" || choice == "4" || choice == "0"));
-
-                            switch (choice)
-                            {
-                                case "1":
-                                    path += @"Complete\small\";
-                                    do
-                                    {
-
-                                        Console.WriteLine("1.Case1\t2.Case2");
-                                        Console.Write("Select Test Case: ");
-                                        choice = Console.ReadLine();
-                                    } while (!(choice == "1" || choice == "2"));
-
-
-                                    switch (choice)
-                                    {
-                                        case "1":
-                                            path += @"Case1\";
-                                            moviesPath = path + "Movies193.txt";
-                                            queriesPath = path + "queries110.txt";
-                                            break;
-                                        case "2":
-                                            path += @"Case2\";
-                                            moviesPath = path + "Movies187.txt";
-                                            queriesPath = path + "queries50.txt";
-
-                                            break;
-                                    }
-
-
-                                    break;
-                                case "2":
-                                    path += @"Complete\medium\";
-                                    do
-                                    {
-                                        Console.WriteLine("1.Case1\t2.Case2");
-                                        Console.Write("Select Test Case: ");
-                                        choice = Console.ReadLine();
-                                    } while (!(choice == "1" || choice == "2"));
-
-
-                                    switch (choice)
-                                    {
-                                        case "1":
-                                            path += @"Case1\";
-                                            moviesPath = path + "Movies967.txt";
-                                            queriesPath = path + "queries4000.txt";
-                                            //queriesPath = path + "queries85.txt";
-                                            break;
-                                        case "2":
-                                            path += @"Case2\";
-                                            moviesPath = path + "Movies4736.txt";
-                                            queriesPath = path + "queries2000.txt";
-                                            //queriesPath = path + "queries110.txt";
-                                            break;
-                                    }
-                                    break;
-                                case "3":
-                                    path += @"Complete\large\";
-                                    moviesPath = path + "Movies14129.txt";
-                                    queriesPath = path + "queries600.txt";
-                                    //queriesPath = path + "queries26.txt";
-                                    break;
-                                case "4":
-                                    path += @"Complete\extreme\";
-                                    moviesPath = path + "Movies122806.txt";
-                                    queriesPath = path + "queries200.txt";
-                                    //queriesPath = path + "queries22.txt";
-                                    break;
-                                case "0":
-                                    goto Ask;
-                            }
-                            break;
-                    }
-
-
-
-                }
-        */
         public static void selectTestCase()
         {
             string path = @"E:\Small-World-phenomenon\Testcases\";
@@ -296,6 +194,23 @@ namespace Small_World_Phenomenon
                     moviesPath = path + "Movies122806.txt";
                     queriesPath = path + "queries200.txt";
                     break;
+            }
+        }
+
+        public static void runTestCase(List<KeyValuePair<string, string>> queries)
+        {
+            foreach (KeyValuePair<string,string> kvp in queries)
+            {
+                solve(kvp.Key, kvp.Value);
+            }
+        }
+
+        public static void solve(string who, string whom)
+        {
+            HashSet<string> whoMovies = moviesOfActor[who];
+            foreach(string movie in whoMovies)
+            {
+                
             }
         }
 
